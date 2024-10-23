55efd9bd93a6a08fa4cc239cd69a660f773126a7
GabrielHMA01 <gabriel.martins@sou.inteli.edu.br>
2024-10-23 18:48:55 -0300

    Update semana9.js

--- semana9.js
+++ semana9.js
@@ -1,4 +1,4 @@
-array = ["leo", "gabriel", "isa", "matheus", "pietra"]
+array = ["leonardo", "gabriel", "isa", "matheus", "pietra"]
 for (let i = 0; i < array.length; i++){
     console.log(array[i])
 }
